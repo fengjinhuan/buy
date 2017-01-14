eda9b9b625bc8cf2cf2e9eb27f23c8e113f45843
fengjinhuan <625370628@qq.com>
2017-01-14 18:54:37 +0800

    代理商详情页-外汇返佣

--- pages/dlsxq/js/index.js
+++ pages/dlsxq/js/index.js
@@ -9,6 +9,39 @@ dlsxq.directive('dlsxqCall',function(){
             con:"@con"
         }
     }
+});
+var dlsxqArr = [
+    {
+        id:1,
+        url:'dlsxq.png',
+        money:'8$',
+        love:'有纯ECN PRO的可开工的是',
+        title:'Imsforex'
+    },
+    {
+        id:2,
+        url:'dls_07.png',
+        money:'18$',
+        love:'抱财网 科技Sprovice Disk',
+        title:'Provider'
+    },
+    {
+        id:3,
+        url:'dlsxq.png',
+        money:'2$',
+        love:'你有你的收到货的S provider',
+        title:'configSvo'
+    },
+    {
+        id:4,
+        url:'s9.png',
+        money:'1$',
+        love:'FXCM 你的独特信息定制者及师',
+        title:'SpiansolLov'
+    }
+]
+dlsxq.controller('dlsxq',function($scope){
+    $scope.dlsxqArr=dlsxqArr;
 })
         // id:4,
         // title:'EDG个人工作室',
